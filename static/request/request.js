c493ef7a5df1526345a06edb86a9800ebf11ecd2
Hubert Golan <hubert.golan@computerplus.com.pl>
2017-09-26 15:48:07 +0200

    testing requests

--- static/request/request.js
+++ static/request/request.js
@@ -17,49 +17,44 @@
 //
 // ==/UserScript==
 
-if (typeof(dojo) != "undefined") {
-	require('dojo/domReady', function () {
-		try {
-			// utility function to let us wait for a specific element of the page to load...
-			var waitFor = function (callback, elXpath, elXpathRoot, maxInter, waitTime) {
-				if (!elXpathRoot)
-					var elXpathRoot = dojo.body();
-				if (!maxInter)
-					var maxInter = 10000; // number of intervals before expiring
-				if (!waitTime)
-					var waitTime = 1; // 1000=1 second
-				if (!elXpath)
-					return;
-				var waitInter = 0; // current interval
-				var intId = setInterval(function () {
-						if (++waitInter < maxInter && !dojo.query(elXpath, elXpathRoot).length)
-							return;
+if(typeof(dojo) != "undefined") {
+	 require(['dojo/domReady', "dojo/dom-construct", "dojo/request"], function (domReady, domConstruct, request) {
+        try {
+            // utility function to let us wait for a specific element of the page to load...
+            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if(!elXpathRoot) var elXpathRoot = dojo.body();
+                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if(!waitTime) var waitTime = 1;  // 1000=1 second
+                if(!elXpath) return;
+                var waitInter = 0;  // current interval
+                var intId = setInterval( function(){
+                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
 
-						clearInterval(intId);
-						if (waitInter >= maxInter) {
-							console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
-						} else {
-							console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
-							callback();
-						}
-					}, waitTime);
-			};
+                    clearInterval(intId);
+                    if( waitInter >= maxInter) {
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
+                    } else {
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
+                        callback();
+                    }
+                }, waitTime);
+            };
 
-			// here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
-			// before we proceed to customize the page...
-			waitFor(function () {
-				work()
-			},
-				".lotusStreamTopLoading div.loaderMain.lotusHidden");
-		} catch (e) {
-			alert("Exception occurred in helloWorld: " + e);
-		}
-
-	});
+            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
+            // before we proceed to customize the page...
+            waitFor( function(){
+			// wait until the "loading..." node has been hidden
+			// indicating that we have loaded content.
+   			 work(domConstruct,request);
+       	          },
+		  ".activityStreamPage .lotusContent.lotusBoard");
+      } catch(e) {
+          alert("Exception occurred in helloWorld: " + e);
+      }
+   });
 }
 
-function work() {
-	require(['dojo/domReady', "dojo/dom-construct", "dojo/request"], function (domReady, domConstruct, request) {
+function work(domConstruct,request) {
 		request.get("https://testauth2.eu-gb.mybluemix.net/OAuth2.0_AuthCode_part_3/", {
 			headers: {
 				"X-Requested-With": null,
@@ -81,5 +76,5 @@ function work() {
 				});
 			domConstruct.place(n, dojo.query(".lotusContent")[0]);
 		});
-	});
 }
+
